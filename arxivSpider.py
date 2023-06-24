8b6738a036158d0af47127ed0a3650418b3856c7
WANGJUNJIE <32323900+wanng-ide@users.noreply.github.com>
2023-06-24 19:48:14 +0900

    Update arxivSpider.py

--- arxivSpider.py
+++ arxivSpider.py
@@ -12,6 +12,7 @@ import argparse
 import itertools
 from fake_useragent import UserAgent
 from concurrent.futures import as_completed
+from bs4 import BeautifulSoup
 
 # 创建一个用户代理池
 ua = UserAgent()
@@ -20,21 +21,57 @@ ua = UserAgent()
 session = requests.Session()
 
 # 创建一个URL列表
-url_list = ['http://cn.arxiv.org/', 'http://export.arxiv.org/']
+url_list = ['http://cn.arxiv.org/', 'http://export.arxiv.org/', 'http://de.arxiv.org/']
+# 'http://de.arxiv.org/'
 
-def crawl(url, headers=None):
+def get_pdf_link(url):
     headers = {'User-Agent': ua.random}
     try:
-        # 使用Session对象发送请求
-        resp = session.get(url, timeout=30)
-        for i in range(retry_times):
-            if resp.status_code == 200: break
-            resp = session.get(url, headers=headers, timeout=30)
-            time.sleep(3) # 如果爬太快被反爬，就把这一行的注释去掉
-        return resp
-    except requests.exceptions.ConnectionError as e:
-        logger.error(f"Connection error: {e}")
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 在arXiv网站中，PDF链接通常在"Download"部分下的"PDF"链接
+        pdf_link = soup.find('a', string='PDF')['href']
+
+        # 获取输入URL的前缀，然后将其添加到PDF链接前面
+        url_prefix = url.split('/abs')[0]
+        pdf_url = url_prefix + pdf_link
+
+        return pdf_url
+    except Exception as e:
+        logger.error(f"Error in get_pdf_link: {e}, URL: {url}")
         return None
+
+def crawl(pid, file_type, headers=None):
+    headers = {'User-Agent': ua.random}
+    # 根据file_type构造URL的后缀
+    url_suffix = 'e-print/' if file_type == 'source' else 'abs/'
+    for base_url in url_list:
+        try:
+            # 构造URL
+            url = base_url + url_suffix + pid
+            # 如果我们正在下载PDF文件，我们需要使用get_pdf_link函数获取URL
+            if file_type == 'pdf':
+                pdf_url = get_pdf_link(url)
+                if pdf_url is None:
+                    continue
+                resp = session.get(pdf_url, timeout=30)
+            else:
+                resp = session.get(url, timeout=30)
+            for i in range(retry_times):
+                if resp.status_code == 200: break
+                resp = session.get(url, headers=headers, timeout=30)
+                time.sleep(4) # 如果爬太快被反爬，就把这一行的注释去掉
+            return resp
+        except requests.exceptions.ConnectionError as e:
+            logger.error(f"Connection error: {e}, URL: {url}")
+            # 如果我们已经尝试了所有的URL，那么我们返回None
+            if base_url == url_list[-1]:
+                return None
+            # 否则，我们继续下一个URL
+            continue
+        
+    return None
 
 def download_files(j, out_folder):
     global done_set
@@ -47,11 +84,13 @@ def download_files(j, out_folder):
     # pdf_url = 'https://arxiv.org/pdf/' + pid
     # page_url = 'https://arxiv.org/abs/' + pid
     
-    # 随机选择一个URL
-    base_url = random.choice(url_list)
-    source_url = base_url + 'e-print/' + pid
-    pdf_url = base_url + 'pdf/' + pid
-    page_url = base_url + 'abs/' + pid
+    # # 随机选择一个URL
+    # base_url = random.choice(url_list)
+    # source_url = base_url + 'e-print/' + pid
+    # page_url = base_url + 'abs/' + pid
+    # pdf_url = get_pdf_link(page_url)
+    pdf_url = None
+    source_url = None
     
     # 生成输出目录
     pdf_out_folder = os.path.join(out_folder, pid.replace('/', '_'),'pdf') # 带有replace的部分是因为有的pid里存在 '/' 符号
@@ -68,26 +107,30 @@ def download_files(j, out_folder):
         return
     
     # 下载pdf文件
-    logger.info(f'Downloading PDF file {pid}')
-    pdf_resp = crawl(pdf_url)
+    # logger.info(f'Downloading PDF file {pid}')
+    pdf_resp = crawl(pid, 'pdf')
     pdf_status = pdf_resp.status_code if pdf_resp else None
     # pdf_status = pdf_resp.status_code
     # 下载source文件
-    logger.info(f'Downloading source file {pid}')
-    source_resp = crawl(source_url)
+    # logger.info(f'Downloading source file {pid}')
+    source_resp = crawl(pid, 'source')
     # source_status = source_resp.status_code
     source_status = source_resp.status_code if source_resp else None
     
     # 保存文件
     if pdf_status == 200:
         os.makedirs(pdf_out_folder, exist_ok=True)
+        pdf_url = pdf_resp.url  # 记录下载的PDF文件的URL
         with open(pdf_out_path, 'wb')as wb: wb.write(pdf_resp.content)
+        logger.info(f"Successfully downloaded PDF file {pid}")  
     else: 
         pdf_out_path = ""
         logger.error(f"Failed to download PDF file {pid}")
     if source_status == 200:
         os.makedirs(source_out_folder, exist_ok=True)
+        source_url = source_resp.url  # 记录下载的源文件的URL
         with open(source_out_path, 'wb')as wb: wb.write(source_resp.content)
+        logger.info(f"Successfully downloaded source file {pid}")
     else: 
         source_out_path = ""
         logger.error(f"Failed to download source file {pid}")
@@ -98,13 +141,15 @@ def download_files(j, out_folder):
     # 保存下载记录
     save_info = dict()
     save_info['id'] = pid
-    save_info['arxiv_url'] = page_url
+    save_info['arxiv_url'] = 'https://arxiv.org/abs/' + pid
     save_info['title'] = j['title']
     save_info['autorhs'] = j.get('authors_parsed', False) or j['authors']
     save_info['paper_time'] = j['update_date']
     save_info['download_time'] = time.strftime('%Y-%m-%d')
     save_info['pdf_path'] = pdf_out_path
     save_info['source_path'] = source_out_path
+    save_info['pdf_url'] = pdf_url  # 添加PDF文件的URL
+    save_info['source_url'] = source_url  # 添加源文件的URL
     save_info['pdf_status'] = pdf_status
     save_info['source_status'] = source_status
     with jsonlines.open(log_file, mode='a') as writer:
